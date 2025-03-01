da2299178f79ec96d25c5921aca161afb4239523
wellynton.spagnol <wellyntonspagnol10@gmail.com>
2025-03-01 01:39:30 -0300

    feat: Refatoração do Product

--- src/main/java/com/example/dummyjson/dto/Product.java
+++ src/main/java/com/example/dummyjson/dto/Product.java
@@ -1,9 +1,8 @@
 package com.example.dummyjson.dto;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public class Product {
 
@@ -20,8 +19,6 @@ public class Product {
 
     @NotNull
     private Double price;
-
-    // Getters and Setters
 
     public Long getId() {
         return id;
