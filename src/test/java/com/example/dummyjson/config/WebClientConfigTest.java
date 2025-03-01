dffb0c91cbdcf715aeb9f548b67814d017f4410f
wellynton.spagnol <wellyntonspagnol10@gmail.com>
2025-03-01 02:27:34 -0300

    fit:Atualizacao do RestTemplateConfigTest para o WebClientConfigTest

---
+++ src/test/java/com/example/dummyjson/config/WebClientConfigTest.java
@@ -0,0 +1,30 @@
+package com.example.dummyjson.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class WebClientConfigTest {
+
+    @Autowired
+    private WebClient webClient;
+
+    @Value("${api.base.url}")
+    private String baseUrl;
+
+    @Test
+    public void testWebClientBean() {
+        assertNotNull(webClient, "O WebClient não deve ser nulo");
+
+        assertEquals(
+                "https://dummyjson.com/products",
+                baseUrl,
+                "A URL base não corresponde ao esperado");
+    }
+}
